3597d5cf01a66c0f082e805f1a381c08011b244b
assembly3 <81543759+assembly3@users.noreply.github.com>
2023-09-10 18:42:56 -0400

    Add files via upload

---
+++ New folder4/Maven_Project - Copy(1)/src/main/java/GUIs/Class1.java
@@ -0,0 +1,16 @@
+package GUIs;
+
+import javax.swing.*;
+
+public class Class1 {
+
+    public static void main(String[] args){
+    JFrame frame = new JFrame("GUI1");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(300,300);
+    JButton button = new JButton("GUI1");
+                frame.getContentPane().add(button);
+                frame.setVisible(true);
+}
+
+}
